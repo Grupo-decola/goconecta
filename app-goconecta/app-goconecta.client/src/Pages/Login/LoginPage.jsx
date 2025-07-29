67cff08aed4fe533b622bcf9300c7a2594159e60
Vitor Marques Rodrigues <v.marques.rodrigues@avanade.com>
2025-07-29 13:09:02 -0300

    feat(router): add login page route

---
+++ app-goconecta/app-goconecta.client/src/Pages/Login/LoginPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function LoginPage() {
+  return (
+    <div>
+      <h1>Página de Login</h1>
+      <LoginForm />
+    </div>
+  );
+}
+
+export default LoginPage;
--- app-goconecta/app-goconecta.client/src/Router.jsx
+++ app-goconecta/app-goconecta.client/src/Router.jsx
@@ -1,9 +1,25 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Cadastro from "./Pages/Cadastro/CadastroLogin";
+import LoginPage from "./Pages/Login/LoginPage";
 import InfoPage from "./Pages/InfoPage/InfoPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
+    element: <InfoPage />,
+  },
+  {
+    path: "/cadastro",
+    element: <Cadastro />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+
+  {
+    path: "/pacote/:id",
     element: <InfoPage />,
   },
 ]);
