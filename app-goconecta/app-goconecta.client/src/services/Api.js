17cd97de6e0068b9c82545842abdf841d9b994c7
Naiara <oliveiranaiara100@gmail.com>
2025-07-29 16:35:01 -0300

    refactor(Api): clean up comments and improve code readability

--- app-goconecta/app-goconecta.client/src/services/Api.js
+++ app-goconecta/app-goconecta.client/src/services/Api.js
@@ -4,12 +4,11 @@ const API_BASE_URL = 'https://localhost:7093/Api';
 
 export const fetchPackages = async () => {
   try {
-    // Faça a requisição GET para o seu endpoint de pacotes no C#
-    const response = await axios.get(`${API_BASE_URL}/packages`); // Supondo que seu endpoint seja /api/packages
-    return response.data; // Os dados que vêm do seu back-end C#
+    const response = await axios.get(`${API_BASE_URL}/packages`); 
+    return response.data; 
   } catch (error) {
     console.error('Erro ao buscar pacotes da API:', error);
-    // Você pode lançar o erro novamente para que o componente Packages.jsx o capture
+  
     throw error;
   }
 };
