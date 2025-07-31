51bb9b5e44ecca784c733594c579aa86af0b25e7
Vitor Marques Rodrigues <v.marques.rodrigues@avanade.com>
2025-07-31 16:12:21 -0300

    feat(ui): reorganize layout structure in App component refactor(components): update ImageCarousel to accept images prop refactor(components): modify PackageCard to use image object properties feat(auth): enhance Cadastro and Login pages with authentication check

--- app-goconecta/app-goconecta.client/src/Pages/Login/LoginPage.jsx
+++ app-goconecta/app-goconecta.client/src/Pages/Login/LoginPage.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import LoginForm from "../../Components/Login/LoginForm"; // Caminho correto
+import { useAuth } from "../../Context/AuthContext";
+import { Text } from "@mantine/core"; // Certifique-se de que o mantine
 
 function LoginPage() {
+  const { isAuthenticated } = useAuth(); // Certifique-se de que useAuth está importado corretamente
   return (
     <div>
-      <LoginForm />
+      {isAuthenticated ? <Text>Você já está autenticado!</Text> : <LoginForm />}
     </div>
   );
 }
