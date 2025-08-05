02c366a34c93746e7bda621a38430c0ab8269acd
Vitor Marques Rodrigues <v.marques.rodrigues@avanade.com>
2025-08-05 14:35:19 -0300

    feat(package-card): add clock icon and improve card layout

--- app-goconecta/app-goconecta.client/src/Pages/Anuncie-Cadastro/Cadastro.jsx
+++ app-goconecta/app-goconecta.client/src/Pages/Anuncie-Cadastro/Cadastro.jsx
@@ -1,17 +1,17 @@
-import React, { useState } from 'react';
-import './Cadastro.css';
-import axios from "axios";
-import {api} from "@/api.js"; 
+import React, { useState } from "react";
+import "./Cadastro.css";
+import { api } from "@/api.js";
+import { notifications } from "@mantine/notifications";
 
 function CadastroPropriedade() {
   const [formData, setFormData] = useState({
-    nomePropriedade: '',
-    descricao: '',
-    endereco: '',
-    cidade: '',
-    estado: '',
-    precoPorNoite: '',
-    tipoPropriedade: '',
+    nomePropriedade: "",
+    descricao: "",
+    endereco: "",
+    cidade: "",
+    estado: "",
+    precoPorNoite: "",
+    tipoPropriedade: "",
     imagem: null,
   });
 
@@ -25,40 +25,58 @@ function CadastroPropriedade() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    
-    let formDataCompiled = {
+
+    const formDataCompiled = {
       Name: formData.nomePropriedade,
       Description: formData.descricao,
       Rating: 0,
       Region: `${formData.cidade}/${formData.estado}`,
       Address: formData.endereco,
-    }
-    
-    api.post('Hotels', formDataCompiled)
-      .then(r => {
-        if (r.status === 201) {
-          console.log("Propriedade cadastrada com sucesso!");
-        }
-        else {
-          console.error("Erro ao cadastrar propriedade:", r.statusText);
-        }
-      }
-    );
+    };
+
+    api
+      .post("Hotels", formDataCompiled)
+      .then(() => {
+        notifications.show({
+          title: "Cadastro realizado!",
+          message: "Propriedade cadastrada com sucesso!",
+          color: "success",
+          autoClose: 4000,
+        });
+      })
+      .catch(() => {
+        notifications.show({
+          title: "Erro ao cadastrar",
+          message:
+            "Erro ao conectar com o servidor. Tente novamente mais tarde.",
+          color: "error",
+          autoClose: 4000,
+        });
+      });
   };
 
-  
-return (
+  return (
     <div className="form-container">
       <h2>Anuncie sua Propriedade</h2>
       <form onSubmit={handleSubmit} enctype="multipart/form-data">
         <label>Nome da Propriedade:</label>
-        <input type="text" name="nomePropriedade" onChange={handleChange} required />
+        <input
+          type="text"
+          name="nomePropriedade"
+          onChange={handleChange}
+          required
+        />
 
         <label>Descrição:</label>
         <textarea name="descricao" onChange={handleChange} required />
 
         <label>Imagem da Propriedade:</label>
-        <input type="file" name="imagem" accept="image/*" onChange={handleChange} />
+        <input
+          type="file"
+          name="imagem"
+          accept="image/*"
+          onChange={handleChange}
+        />
 
         <label>Endereço:</label>
         <input type="text" name="endereco" onChange={handleChange} required />
@@ -70,7 +88,12 @@ return (
         <input type="text" name="estado" onChange={handleChange} required />
 
         <label>Preço por Noite (R$):</label>
-        <input type="number" name="precoPorNoite" onChange={handleChange} required />
+        <input
+          type="number"
+          name="precoPorNoite"
+          onChange={handleChange}
+          required
+        />
 
         <label>Tipo de Propriedade:</label>
         <select name="tipoPropriedade" onChange={handleChange} required>
