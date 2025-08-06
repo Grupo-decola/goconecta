b28953e3d602c68d860cd17a4d1a7cdbd199e26f
williamlimasilva <williamtakuma@yahoo.com.br>
2025-08-06 11:03:37 -0300

    refactor(amenity): remove unused attributes and clean up code

--- app-goconecta/app-goconecta.Server/Models/Amenity.cs
+++ app-goconecta/app-goconecta.Server/Models/Amenity.cs
@@ -1,13 +1,9 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace app_goconecta.Server.Models;
 
 public class Amenity
 {
     public int Id { get; set; }
-    
-    [Display(Name = "Nome")]
     public string Name { get; set; }
-    
     public ICollection<Hotel> Hotels { get; set; } = new List<Hotel>();
 }
+
